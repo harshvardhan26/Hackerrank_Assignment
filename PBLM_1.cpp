2048626d9ab6c3bf8db96b0fc702b4c29b7e246e
harshvardhan26 <63010481+harshvardhan26@users.noreply.github.com>
2020-06-09 11:37:22 +0530

    Add files via upload

---
+++ PBLM_1.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin>>a;
+    while(a--)
+    {
+        int n;
+        cin>>n;
+        for(int i=0;i<n/2;i++)
+        {
+            if(i%2==0)
+            {
+                for(int j=0;j<n-(i+1);j++)
+                    cout<<"%";
+                for(int j=0;j<(i+1);j++)
+                    cout<<"#";
+                cout<<"\n";
+            }
+            else
+            {
+                for(int j=0;j<(i+1);j++)
+                    cout<<"#";
+                for(int j=0;j<n-(i+1);j++)
+                    cout<<"%";
+                cout<<"\n";
+            }
+        }
+        for(int i=n/2;i<n;i++)
+        {
+            if(i%2==0)
+            {
+                for(int j=0;j<i;j++)
+                    cout<<"%";
+                for(int j=0;j<n-i;j++)
+                    cout<<"#";
+                cout<<"\n";
+            }
+            else
+            {
+                for(int j=0;j<n-i;j++)
+                    cout<<"#";
+                for(int j=0;j<i;j++)
+                    cout<<"%";
+                cout<<"\n";
+            }
+        }
+    }
+    return 0;
+}
